036891d96eb797340c49da3ea169f8ba7474c3ba
Changsheng Liu <liucs90@hotmail.com>
2019-02-24 21:25:19 -0800

    make the whole logic runnable

--- hw5/proxy.c
+++ hw5/proxy.c
@@ -4,7 +4,11 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include "util.h"
+
+#define BUFFER_SIZE 1024
 
 void checkParam(int argc, char* argv[]);
 
@@ -22,28 +26,80 @@ int main(int argc, char *argv[])
     if (setsockopt(proxy_sock, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int)) < 0) {
         failHandler("create socket error!");
     }
-    struct sockaddr_in serveraddr;
-    bzero((char *) &serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET; 
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    serveraddr.sin_port = htons(port); 
-    if (bind(proxy_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0) {
-        failHandler("create socket error!");
+    struct sockaddr_in addr;
+    bzero((char *) &addr, sizeof(addr));
+    addr.sin_family = AF_INET; 
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    addr.sin_port = htons(port); 
+    if (bind(proxy_sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        failHandler("bind socket error!");
     }
 
     while(1) {
         if(listen(proxy_sock, 1) < 0){
-            failHandler("listen error!");
+            close(proxy_sock);
+            failHandler("listen socket error!");
         }
-        int addr_len = sizeof(serveraddr);
-        int server_sock = accept(proxy_sock, (struct sockaddr*)&serveraddr, (socklen_t *) &addr_len);
-        if(server_sock < 0) {
+        int addr_len = sizeof(addr);
+        int server_sock;
+        if((server_sock = accept(proxy_sock, (struct sockaddr*)&addr, (socklen_t *) &addr_len)) < 0) {
             continue;
         }
-        char buf[1024];
-        bzero(buf, 1024);
-        write(server_sock, buf, 1024 );
-        fputs(buf, stdout);
+        char browser_buf[BUFFER_SIZE];
+        bzero(browser_buf, BUFFER_SIZE);
+        read(server_sock, browser_buf, BUFFER_SIZE);
+        
+        int host_sock;
+        if((host_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+            failHandler("create socket error!");
+        }
+        
+        int ret = 0;
+
+        char method[20];
+        char request[100];
+        char version[20];
+        sscanf(browser_buf, "%s %s %s", method, request, version);
+        while(strlen(browser_buf) > ret){
+            if(browser_buf[ret] != '\r') {
+                ret++;
+            }else {
+                break;
+            }
+        }
+
+        char title[30];
+        char host[1024];
+        sscanf(browser_buf + ret, "%s %s", title, host);
+        while(strlen(browser_buf) > ret){
+            if(browser_buf[ret] != '\r') {
+                ret++;
+            }else {
+                break;
+            }
+        };
+
+        struct hostent *hp;
+        hp = gethostbyname(host);
+        struct sockaddr_in hostaddr;
+        bzero((char *)&hostaddr, sizeof(hostaddr));
+        hostaddr.sin_family = AF_INET; 
+        bcopy((char *)hp->h_addr_list[0],(char *)&hostaddr.sin_addr.s_addr, hp->h_length);
+        hostaddr.sin_port = htons(80);
+
+        addr_len = sizeof(hostaddr);
+        if(connect(host_sock, (struct sockaddr*)&hostaddr, addr_len) < 0) {
+            failHandler("connect host socket error!");
+        }
+        write(host_sock, browser_buf, BUFFER_SIZE);
+
+        char host_buffer[BUFFER_SIZE];
+        bzero(host_buffer, BUFFER_SIZE);
+        while(read(host_sock, host_buffer, BUFFER_SIZE) > 0) {
+            write(server_sock, host_buffer, BUFFER_SIZE);
+        }
+        close(host_sock);
+        close(server_sock);
     }
 
     return 0;
